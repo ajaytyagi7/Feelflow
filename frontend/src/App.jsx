e1fad0baa62d475368cbf99ecdf27fdbeb4ce2cb
Ajay Kumar Tyagi <151626208+ajaytyagi7@users.noreply.github.com>
2024-04-29 14:38:56 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import {BrowserRouter,Route,Routes,NavLink,Link} from 'react-router-dom'
+import Home from './Component/Home'
+import Navbar from './Component/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='Home' element={<Home/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Component/Home.jsx
@@ -0,0 +1,171 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container-fluid'>
+        <header className='header'>
+            <div className='py-5 mx-auto container-fluid'>
+                <h1 className='text-white  fw-bold display-4 text-center mt-5 '>Empowering Schools with <br />
+                Innovative Solutions</h1>
+                <p className='text-center text-white fs-5'>India's Leading Cloud-Based School Management Software</p>
+                <div className="row home-btn-chat">
+                    <div className="col-md-2">
+                        <button className='btn btn-primary'>30 Days Trail</button>
+                    </div>
+                    <div className="col-md-1">
+                        <button className='btn btn-danger'>Let's Chat</button>
+                    </div>
+
+                </div>
+                <img src="https://feeflow.in/assets/img/herox@2x.png" alt="" />
+            </div>
+
+        </header>
+        <div className="container-fluid">
+            <h3 className='text-center'>22+ Key Features & Modules</h3>
+            <p className='text-center text-secondary fs-5'>Discover how these features enhance your experience, <br />
+             streamline tasks, and provide valuable functionality,<br />
+              enabling you to achieve your goals efficiently.</p>
+            <div className="row">
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'>Student Management </h4>
+                <p className='text-secondary fw-bold text-center'>Effectively manage student fees, track payments, and generate financial reports related to student accounts.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'>Parents Management </h4>
+                <p className='text-secondary fw-bold text-center'>Collaboration with parents, providing them with access to their child's progress reports, attendance records, and important updates.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'>Staff Management </h4>
+                <p className='text-secondary fw-bold text-center'>Effectively manage staff details, including personal information, qualifications, and employment history, ensuring smooth administration and organization.</p> 
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Teacher Management</h4>
+                <p className='text-secondary fw-bold text-center'>Simplify teacher-related tasks, such as tracking teaching assignments, scheduling, performance evaluations, and professional development.</p> 
+                 </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Attachments Book Management</h4>
+                <p className='text-secondary fw-bold text-center'>Easily manage and track the availability and borrowing of books and other educational resources in the school library</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Attendance Management</h4>
+                <p className='text-secondary fw-bold text-center'>Streamline attendance tracking and reporting, ensuring accurate records and facilitating efficient monitoring of student attendance.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Bulk SMS and Email Management</h4>
+                <p className='text-secondary fw-bold text-center'>Facilitate communication with various stakeholders by enabling the sending of bulk SMS and emails, allowing for quick and effective information dissemination.</p> 
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> ID Card Management</h4>
+                <p className='text-secondary fw-bold text-center'>Manage student identification cards, ensuring easy access control and efficient tracking of student movements within the school premises.</p> 
+                 </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'>Certificate Management </h4>
+                <p className='text-secondary fw-bold text-center'>Streamline the process of issuing and managing certificates for various achievements and qualifications, ensuring accurate and organized record-keeping</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Events Management </h4>
+                <p className='text-secondary fw-bold text-center'>Organize and manage school events, such as parent-teacher meetings, sports days, and cultural activities, to ensure smooth planning and execution.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Homework Management</h4>
+                <p className='text-secondary fw-bold text-center'>Simplify the assignment and submission of homework, facilitating effective communication between teachers, students, and parents.</p> 
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Hostel Management</h4>
+                <p className='text-secondary fw-bold text-center'>Efficiently manage hostel facilities, including room allocation, inventory management, and student check-ins and check-outs.</p> 
+                 </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'>Human Resource Management </h4>
+                <p className='text-secondary fw-bold text-center'>Streamline HR-related tasks, including employee information management, attendance tracking, leave management, and payroll processing.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Library Management</h4>
+                <p className='text-secondary fw-bold text-center'>Digitally manage the school library, including cataloging books, tracking borrowing and returns, and generating reports on library usage.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Live Class Management</h4>
+                <p className='text-secondary fw-bold text-center'>Facilitate virtual learning by providing tools for conducting live online classes, including video conferencing, screen sharing, and interactive features.</p> 
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'>Office Accounting Management </h4>
+                <p className='text-secondary fw-bold text-center'>Simplify financial management by tracking expenses, generating invoices, and managing payroll, ensuring accurate and efficient accounting processes.</p> 
+                 </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Students Accounting Management</h4>
+                <p className='text-secondary fw-bold text-center'>Effectively manage student fees, track payments, and generate financial reports related to student accounts.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'>Online Exam Management </h4>
+                <p className='text-secondary fw-bold text-center'>Conduct and manage online examinations, including question paper creation, grading, and result generation, providing a seamless exam experience.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Reception Management</h4>
+                <p className='text-secondary fw-bold text-center'>Streamline front desk operations, including visitor management, appointment scheduling, and inquiries handling, ensuring efficient reception services.</p> 
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Transport Management</h4>
+                <p className='text-secondary fw-bold text-center'>Manage school transportation, including route planning, vehicle tracking, and student transportation scheduling, ensuring safe and reliable transportation services.</p> 
+                 </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> School Website</h4>
+                <p className='text-secondary fw-bold text-center'>Enhance the school's website with customizable modules to showcase information, news, events, and important updates to visitors and parents.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> WhatsApp Integration</h4>
+                <p className='text-secondary fw-bold text-center'>Seamlessly integrate WhatsApp communication into your school management system, allowing for efficient communication with staff, and students.</p>
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> Multi Language Software</h4>
+                <p className='text-secondary fw-bold text-center'>A powerful multilingual school management solution that enables efficient management of student, staff, and parent information.</p> 
+                </div>
+                <div className="col-md-3">
+                <img className='img-fluid mx-auto' src="" alt="" />
+                <h4 className='text-center'> In-built Email Communication</h4>
+                <p className='text-secondary fw-bold text-center'>Efficiently manage communication within your school management system through a comprehensive mailbox feature. Stay connected connections.</p> 
+                 </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
